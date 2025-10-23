13f804945ec284123fba79a10a920002f2ee3a24
ThomasPaul01 <thomas.paul7794@gmail.com>
2025-10-23 14:59:14 +0200

    Add rotation functionality to PlayerController for smoother movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 8;
+    public float rotationSpeed = 720;
+
     public InputActionReference horizontalAction;
     public InputActionReference verticalAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +20,12 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDir = new Vector3(horizontalAction.action.ReadValue<float>(), 0, verticalAction.action.ReadValue<float>());
         this.transform.position += moveDir.normalized * speed * Time.deltaTime;
 
+        if (moveDir != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg + 180f;
+            Quaternion targetRotation = Quaternion.Euler(90, targetAngle, 0);
+            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 
 }
