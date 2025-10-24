48bb3d0c0740688f8e7fa485c99f3cc815318d65
ThomasPaul01 <thomas.paul7794@gmail.com>
2025-10-24 14:32:33 +0200

    Refactor PlayerController to use Rigidbody for movement and rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,21 +13,33 @@ public class PlayerController : MonoBehaviour
     public float inputMultiplier = 1f;
     public bool isFrozen = false;
 
+    private Rigidbody _rb;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        if (_rb != null)
+        {
+            _rb.freezeRotation = true;
+        }
+    }
+
+    void FixedUpdate()
     {
         if (isFrozen) return;
-
+        
         float horizontal = horizontalAction.action.ReadValue<float>() * inputMultiplier;
         float vertical = verticalAction.action.ReadValue<float>() * inputMultiplier;
         Vector3 moveDir = new Vector3(horizontal, 0, vertical);
-        this.transform.position += moveDir.normalized * speed * Time.deltaTime;
+        
+        _rb.MovePosition(_rb.position + moveDir.normalized * speed * Time.fixedDeltaTime);
+        
         if (moveDir != Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg + 180f;
             Quaternion targetRotation = Quaternion.Euler(90, targetAngle, 0);
-            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            Quaternion newRotation = Quaternion.RotateTowards(_rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            _rb.MoveRotation(newRotation);
         }
 
         // Verifier si le joueur est en dessous d'une valeure y
